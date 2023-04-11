f7982f415cb2e652bb08a2915e03aa36cb76a828
vavald <valentin.duprez@student.kuleuven.be>
2023-04-11 15:24:07 +0200

    hey

--- src/routes/02_exercises/02_basic_visualisations/exercise_17/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_17/Exercise.svelte
@@ -7,12 +7,14 @@
     const innerHeight = height - margin.top - margin.bottom;
   
     // Array
+    import { scaleLog } from "d3-scale";
     const values = [2, 4, 6, 7, 9];
+    const scale = scaleLog(values)
   </script>
   
   <svg viewBox="0 0 {width} {height}">
     <g transform="translate({margin.left},{margin.top})">
-      <!--  -->
+      <circle cx=10 cy=10 r=10 transform={scale}/>
     </g>
   </svg>
   
